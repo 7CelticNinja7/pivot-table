fb62dc3a1af97f44818ff80df070580062c2c001
Philip Lengefeld <philiplengefeld@gmail.com>
2023-06-15 13:23:27 -0400

    added recursive attempt

---
+++ src/App-recursive.js
@@ -0,0 +1,114 @@
+import './App.css';
+import sales from "./UI Take Home Test - sales-orders.json";
+import PivotTable from './Components/PivotTable';
+
+function App() {
+  const rowDimensions = ["category", "subCategory", "region"];
+
+  const rowDimensionsDisplayNames = [
+   { value: "orderId", display: "Order ID" },
+   { value: "orderDate", display: "Order Date" }, 
+   { value: "shipDate", display: "Ship Date" }, 
+   { value: "shipMode", display: "Ship Mode" }, 
+   { value: "customerId", display: "Customer ID" }, 
+   { value: "customerName", display: "Customer Name" },
+   { value: "segment", display: "Segment" },
+   { value: "country", display: "Country" },
+   { value: "city", display: "City" }, 
+   { value: "state", display: "State" }, 
+   { value: "postalCode", display: "Postal Code" }, 
+   { value: "region", display: "Region" },
+   { value: "productId", display: "Product ID" }, 
+   { value: "category", display: "Category" }, 
+   { value: "subCategory", display: "Sub-Category" }, 
+   { value: "productName", display: "Product Name" }, 
+   { value: "sales", display: "Sales" },
+   { value: "quantity", display: "Quantity" }, 
+   { value: "discount", display: "Discount" },
+   { value: "profit", display: "Profit" }, 
+  ]
+
+  const rowDimensionsToDisplay = rowDimensions.map(rowDimension => {
+    const dimensionToDisplay = rowDimensionsDisplayNames.filter(dimension => {
+      return dimension.value === rowDimension;
+    })
+    return dimensionToDisplay[0].display;
+  })
+
+  const states = Array.from(new Set(sales.map(saleItem => {
+    return saleItem.state;
+  })));
+
+  const getSalesByCategory = (category, rowDimension) => {
+    const stateSalesBySubCategory = {};
+    for (let i = 0; i < states.length; i++) {
+      stateSalesBySubCategory[states[i]] = sales.filter(saleItem => {
+          return saleItem[rowDimension] === category && saleItem.state === states[i];
+        }).reduce((aggregator, stateSaleItem) => aggregator + stateSaleItem.sales, 0)
+    };
+    return stateSalesBySubCategory;
+  }
+
+  // const getSubCategories = (category) => {
+  //   const salesByCategory = {};
+  //   const subCategories = Array.from(new Set(sales.filter(saleItem => {
+  //     return saleItem[rowDimensions[0]] === category;
+  //   }).map(filteredSaleItem => {
+  //     return filteredSaleItem[rowDimensions[1]];
+  //   })));
+
+  //   for (let i = 0 ; i < subCategories.length; i++) {
+  //     salesByCategory[subCategories[i]] = getSalesByCategory(subCategories[i], rowDimensions[1]);
+  //   }
+  //   salesByCategory.Total = getSalesByCategory(category, rowDimensions[0]);
+  //   return salesByCategory;
+  // }
+
+  const mainCategories = Array.from(new Set(sales.map(saleItem => { return saleItem[rowDimensions[0]]})));
+  
+  
+  // for (let i = 0; i < categories.length; i++) {
+  //   mappedCategories[categories[i]] = getSubCategories(categories[i]);
+  // };
+  // For each row dimension,
+    // category
+        // subcategory
+          // sub-subcategory
+          // sub-subcategory
+        // subcategory
+          // sub-subcategory
+          // sub-subcategory
+  // when no sub-category remains, get sale data for that category
+
+  const mainCategory = rowDimensions[0];
+  const getSalesByCategoryRecursive = (categories, rowDimensions) => {
+    const dimension = rowDimensions.shift();
+    const category = categories.shift();
+    if (rowDimensions.length > 0) {
+      return { [category]: getSalesByCategoryRecursive(categories, rowDimensions) };
+    } else {
+      return { [category]: getSalesByCategory(mainCategory, category, dimension) };
+    }     
+  }
+  const mappedCategories = getSalesByCategoryRecursive(mainCategories, rowDimensions);
+  console.log("🚀 ~ file: App.js:94 ~ App ~ mappedCategories:", mappedCategories)
+  debugger;
+
+  
+  
+  const pivotTableProps = {
+    MappedCategories: mappedCategories,
+    States: states,
+    RowDimensions: rowDimensionsToDisplay,
+  }
+
+
+
+  return (    
+    <div>
+      <PivotTable {...pivotTableProps}></PivotTable>
+    </div>
+  );
+}
+
+export default App;
